0d7291a277774a7c79d325313a1eed19a78d05c8
Damian <olebueziedamian@gmail.com>
2024-07-29 11:00:20 +0100

    updated daily rewards

--- app/Admin/new-question/CreateQuestQuestions.jsx
+++ app/Admin/new-question/CreateQuestQuestions.jsx
@@ -82,85 +82,87 @@ export default function CreateQuestQuestion() {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-xl">
-      <div className="flex justify-end mr-4 mt-6">
+    <section>
+      <div className="flex justify-end mr-4 mt-20">
         <SideNav />
       </div>
-      <h2 className="text-2xl font-bold mb-6 text-yellow-600">
-        Add Quest Question
-      </h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="questSelect" className="block text-gray-700 mb-2">
-            Select Quest
-          </label>
-          <select
-            id="questSelect"
-            value={selectedQuestId}
-            onChange={(e) => setSelectedQuestId(e.target.value)}
-            required
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500">
-            {quests.map((quest) => (
-              <option key={quest._id} value={quest._id}>
-                {quest.questName}
-              </option>
-            ))}
-          </select>
-        </div>
-        {["questImage1", "questImage2", "questImage3", "questImage4"].map(
-          (field) => (
-            <div key={field}>
-              <label htmlFor={field} className="block text-gray-700 mb-2">
-                {field} URL
-              </label>
-              <input
-                type="text"
-                id={field}
-                name={field}
-                value={question[field]}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
-              />
-            </div>
-          )
-        )}
-        <div>
-          <label htmlFor="questHints" className="block text-gray-700 mb-2">
-            Quest Hints
-          </label>
-          <textarea
-            id="questHints"
-            name="questHints"
-            value={question.questHints}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
-          />
-        </div>
-        {question.questPossibleAnswers.map((answer, index) => (
-          <div key={index}>
-            <label
-              htmlFor={`answer${index}`}
-              className="block text-gray-700 mb-2">
-              Possible Answer {index + 1}
+      <div className="max-w-md mx-auto mt-4 p-6 bg-gray-100 rounded-lg shadow-xl">
+        <h2 className="text-2xl font-bold mb-6 text-yellow-600">
+          Add Quest Question
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="questSelect" className="block text-gray-700 mb-2">
+              Select Quest
             </label>
-            <input
-              type="text"
-              id={`answer${index}`}
-              value={answer}
-              onChange={(e) => handleAnswerChange(index, e.target.value)}
+            <select
+              id="questSelect"
+              value={selectedQuestId}
+              onChange={(e) => setSelectedQuestId(e.target.value)}
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500">
+              {quests.map((quest) => (
+                <option key={quest._id} value={quest._id}>
+                  {quest.questName}
+                </option>
+              ))}
+            </select>
+          </div>
+          {["questImage1", "questImage2", "questImage3", "questImage4"].map(
+            (field) => (
+              <div key={field}>
+                <label htmlFor={field} className="block text-gray-700 mb-2">
+                  {field} URL
+                </label>
+                <input
+                  type="text"
+                  id={field}
+                  name={field}
+                  value={question[field]}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                />
+              </div>
+            )
+          )}
+          <div>
+            <label htmlFor="questHints" className="block text-gray-700 mb-2">
+              Quest Hints
+            </label>
+            <textarea
+              id="questHints"
+              name="questHints"
+              value={question.questHints}
+              onChange={handleChange}
               required
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
             />
           </div>
-        ))}
-        <button
-          type="submit"
-          className="w-full bg-yellow-500 text-white py-2 px-4 rounded-md hover:bg-yellow-600 transition duration-300">
-          Add Question
-        </button>
-      </form>
-    </div>
+          {question.questPossibleAnswers.map((answer, index) => (
+            <div key={index}>
+              <label
+                htmlFor={`answer${index}`}
+                className="block text-gray-700 mb-2">
+                Possible Answer {index + 1}
+              </label>
+              <input
+                type="text"
+                id={`answer${index}`}
+                value={answer}
+                onChange={(e) => handleAnswerChange(index, e.target.value)}
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+              />
+            </div>
+          ))}
+          <button
+            type="submit"
+            className="w-full bg-yellow-500 text-white py-2 px-4 rounded-md hover:bg-yellow-600 transition duration-300">
+            Add Question
+          </button>
+        </form>
+      </div>
+    </section>
   );
 }
--- app/daily-reward/page.jsx
+++ app/daily-reward/page.jsx
@@ -1,31 +1,73 @@
 import { BsCalendar2Date } from "react-icons/bs";
 import { FaTasks } from "react-icons/fa";
+import Button from "../components/Reusable/Button";
 import SideNav from "../components/Reusable/SideNav";
 
 const DailyReward = () => {
   return (
     <section>
       <div>
-        <div className="flex justify-between items-center">
-          <h1 className="my-4 px-4">EARN MORE REWARDS!</h1>
+        <div className="flex justify-between w-full items-center px-6">
+          <h1 className="my-4">EARN MORE REWARDS!</h1>
           <SideNav />
         </div>
-        <div className="flex mt-10 items-center justify-between gap-2  rounded-lg mx-4">
-          <div className=" flex flex-col items-center justify-center gap-8 w-40 h-30 p-3 rounded-xl bg-neutral-700 text-yellow-500 hover:bg-neutral-950 cursor-pointer">
+        <div className="flex mt-10 items-center justify-between gap-2  rounded-md mx-4">
+          <div className=" flex flex-col items-center justify-center gap-8 w-32 h-30 py-3 rounded-xl bg-neutral-700 text-yellow-500 hover:bg-neutral-950 cursor-pointer">
             <BsCalendar2Date className="text-3xl " />
             <p className="text-[12px]">Daily Reward</p>
           </div>
-          <div className=" flex flex-col items-center justify-center gap-8 w-40 h-30 p-3 rounded-xl bg-neutral-700 hover:bg-neutral-950 cursor-pointer">
+          <div className=" flex flex-col items-center justify-center gap-8 w-32 h-30 py-3 rounded-xl bg-neutral-700 hover:bg-neutral-950 cursor-pointer">
             <FaTasks className="text-3xl " />
             <p className="text-sm">Daily Tasks</p>
           </div>
-          <div className=" flex flex-col items-center justify-center gap-8 w-40 h-30 p-3 rounded-xl bg-neutral-700 hover:bg-neutral-950 cursor-pointer">
+          <div className=" flex flex-col items-center justify-center gap-8 w-32 h-30 py-3 rounded-xl bg-neutral-700 hover:bg-neutral-950 cursor-pointer">
             <img src="challengeIcon.svg" alt="daily challenge" />
             <p className="text-sm">Challenges</p>
           </div>
         </div>
       </div>
+      <DailyBonus />
     </section>
   );
 };
 export default DailyReward;
+
+const DailyBonus = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-10">
+      <div className="bg-neutral-800 p-6 rounded-lg shadow-lg">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-2">DAILY BONUS</h1>
+          <p className="text-sm mb-6">
+            Free Coins for logging into the game daily without skipping
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-6 mb-6">
+          <div className="flex bg-neutral-900 py-6 flex-col border border-neutral-500 rounded-lg items-center">
+            <img
+              src="/coins.png"
+              alt="Chain Points"
+              className="w-12 h-12 mb-2"
+            />
+            <span className="text-xl mb-2 font-bold">+1200</span>
+            <span className="text-sm">Chain Points</span>
+          </div>
+          <div className="flex bg-neutral-900 py-6 flex-col border border-neutral-500 rounded-lg items-center">
+            <img
+              src="/ticketIcon.svg"
+              alt="Play Passes"
+              className="w-12 h-12 mb-2"
+            />
+            <span className="text-xl font-bold">4</span>
+            <span className="text-sm mb-2">Play Passes</span>
+          </div>
+        </div>
+        <div className="text-center">
+          <Button className="px-20 py-2 bg-yellow-500 hover:bg-yellow-400 active:scale-105 text-black font-bold rounded-md">
+            CLAIM
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
