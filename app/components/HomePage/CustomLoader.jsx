526f512b3cf2cc7bc17bd8d0de60eda78ffa754a
Damian <olebueziedamian@gmail.com>
2024-07-31 07:47:20 +0100

    ui

--- app/components/HomePage/CustomLoader.jsx
+++ app/components/HomePage/CustomLoader.jsx
@@ -10,4 +10,23 @@ const CustomLoader = () => {
   );
 };
 
+export const QuestSkeleton = () => {
+  return (
+    <div className="border border-gray-700 w-72 md:w-72 rounded-xl bg-gray-900 mb-4 flex flex-col justify-between items-center shadow-md animate-pulse px-1">
+      <div className="rounded-xl h-36 w-full bg-gray-700"></div>
+      <div className="flex justify-between items-center w-full px-3 my-2">
+        <div className="w-1/2 h- my-1 bg-gray-700 rounded"></div>
+        <div className="w-1/4 h-4 bg-gray-700 rounded"></div>
+      </div>
+      <div className="mx-6 pb-2 h-4 bg-gray-700 rounded w-full "></div>
+      <div className="flex justify-between gap-2 items-center text-xs px-3 w-full mb-2 my-2">
+        <div className="w-1/3 h-4 bg-gray-700 rounded"></div>
+        <div className="w-1/3 h-4 bg-gray-700 rounded"></div>
+        <div className="w-1/4 h-4 bg-gray-700 rounded"></div>
+      </div>
+    </div>
+  );
+};
+
+
 export default CustomLoader;
--- app/quests/FetchQuests.jsx
+++ app/quests/FetchQuests.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import SideNav from "../components/Reusable/SideNav";
 import Button from "../components/Reusable/Button";
 import Link from "next/link";
+import { QuestSkeleton } from "../components/HomePage/CustomLoader";
 
 const FetchQuestsFromDb = () => {
   const [quests, setQuests] = useState([]);
+  const [loading, setLoading] = useState(true); // Manage loading state
 
   useEffect(() => {
     const fetchQuests = async () => {
@@ -19,6 +21,8 @@ const FetchQuestsFromDb = () => {
         }
       } catch (error) {
         console.error("Error fetching quests:", error);
+      } finally {
+        setLoading(false); // Set loading to false once data is fetched
       }
     };
 
@@ -27,49 +31,53 @@ const FetchQuestsFromDb = () => {
 
   return (
     <section className="bg-gray-900">
-      <h1 className="flex justify-between items-center mx-4 my-4 py-2 text-gray-300">
+      <div className="flex justify-between items-center mx-4 my-4 py-2 text-gray-300">
         <h1 className="font-bold text-4xl">QUESTS</h1>
         <div className="flex gap-2 cursor-pointer">
           <SideNav />
         </div>
-      </h1>
+      </div>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-        {quests.map((quest) => (
-          <Link
-            key={quest._id}
-            href={`/quests/${quest._id}`}
-            className="border border-gray-700 w-72 md:w-72 rounded-xl bg-gray-900 hover:bg-gray-800 mb-4 flex flex-col justify-between items-center shadow-md transition-all duration-300">
-            <img
-              className="rounded-xl h-36 object-cover w-full"
-              src={quest.questImage}
-              alt={quest.questName}
-              onError={(e) => (e.target.src = "/quest/bitcoin.jpg")}
-            />
-            <div className="flex justify-between items-center w-full px-3 my-2">
-              <span className="uppercase font-bold text-gray-100">
-                {quest.questName}
-              </span>
-              <Button className="bg-yellow-300 px-4 py-2 text-black text-xs hover:bg-yellow-200 transition-colors duration-300">
-                {quest.questStatus}
-              </Button>
-            </div>
-            <div className="mx-3 pb-2 text-xs text-center">
-              {quest.questDescription ||
-                "Embark on Word Trails, learn about blockchain - Think, Tap, Win. Earn Tokens and NFTs"}
-            </div>
-            <div className="flex justify-between gap-2 items-center text-xs px-3 w-full mb-2">
-              <span className="border border-gray-700 p-2 rounded-md bg-gray-700 text-gray-300">
-                {quest.questQuestions.length} questions
-              </span>
-              <span className="border border-gray-700 p-2 rounded-md bg-gray-700 text-gray-300">
-                {1000 * quest.questQuestions.length} points
-              </span>
-              <span className="p-2 font-bold text-gray-900 border border-gray-900 rounded-md bg-yellow-400">
-                1 NFT
-              </span>
-            </div>
-          </Link>
-        ))}
+        {loading
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <QuestSkeleton key={index} />
+            ))
+          : quests.map((quest) => (
+              <Link
+                key={quest._id}
+                href={`/quests/${quest._id}`}
+                className="border border-gray-700 w-72 md:w-72 rounded-xl bg-gray-900 hover:bg-gray-800 mb-4 flex flex-col justify-between items-center shadow-md transition-all duration-300">
+                <img
+                  className="rounded-xl h-36 object-cover w-full"
+                  src={quest.questImage}
+                  alt={quest.questName}
+                  onError={(e) => (e.target.src = "/quest/bitcoin.jpg")}
+                />
+                <div className="flex justify-between items-center w-full px-3 my-2">
+                  <span className="uppercase font-bold text-gray-100">
+                    {quest.questName}
+                  </span>
+                  <Button className="bg-yellow-300 px-4 py-2 text-black text-xs hover:bg-yellow-200 transition-colors duration-300">
+                    {quest.questStatus}
+                  </Button>
+                </div>
+                <div className="mx-3 pb-2 text-xs text-center">
+                  {quest.questDescription ||
+                    "Embark on Word Trails, learn about blockchain - Think, Tap, Win. Earn Tokens and NFTs"}
+                </div>
+                <div className="flex justify-between gap-2 items-center text-xs px-3 w-full mb-2">
+                  <span className="border border-gray-700 p-2 rounded-md bg-gray-700 text-gray-300">
+                    {quest.questQuestions.length} questions
+                  </span>
+                  <span className="border border-gray-700 p-2 rounded-md bg-gray-700 text-gray-300">
+                    {1000 * quest.questQuestions.length} points
+                  </span>
+                  <span className="p-2 font-bold text-gray-900 border border-gray-900 rounded-md bg-yellow-400">
+                    1 NFT
+                  </span>
+                </div>
+              </Link>
+            ))}
       </div>
     </section>
   );
