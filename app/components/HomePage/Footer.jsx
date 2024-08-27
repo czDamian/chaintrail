c0e213e30eb7fc0dfe6c67c2942e9fe5564a84c5
Damian <olebueziedamian@gmail.com>
2024-08-27 12:16:17 +0100

    adjusted footer spacing

--- app/components/HomePage/Footer.jsx
+++ app/components/HomePage/Footer.jsx
@@ -5,24 +5,24 @@ import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   const teamMembers = [
     {
-      name: "Okoye Kevin Chibuoyim",
+      name: "Okoye Kevin C.",
       role: "Project Manager",
       image: "/team/photo_2024-07-24_20-18-21.jpg",
       github: "https://github.com/KevinChibuoyim",
-      twitter: "",
-      linkedin: "",
+      twitter: " ",
+      linkedin: " ",
     },
     {
       name: "Damian Olebuezie",
       role: "Smart Contract Developer",
       image: "/team/photo_2024-07-25_02-29-08.jpg",
       github: "https://github.com/czDamian",
-      twitter: "",
-      linkedin: "",
+      twitter: " ",
+      linkedin: " ",
     },
     {
-      name: "Okeke Chinedu Emmanuel",
-      role: "Frontend Developer",
+      name: "Okeke Chinedu E.",
+      role: "Frontend Dev",
       image: "/team/photo_2024-07-24_20-23-00.jpg",
       github: "https://github.com/chiscookeke11",
       twitter: " ",
@@ -33,30 +33,30 @@ const Footer = () => {
       role: "UI/UX Designer",
       image: "/team/Flora.jpg",
       github: "https://x.com/oluwa_chioma",
-      twitter: "",
-      linkedin: "",
+      twitter: " ",
+      linkedin: " ",
     },
     {
       name: "Princewill Emeka",
-      role: "Backend Developer",
+      role: "Backend Dev",
       image: "/team/photo_2024-07-25_02-29-03.jpg",
       github: "https://github.com/Prnzwil",
-      twitter: "",
-      linkedin: "",
+      twitter: " ",
+      linkedin: " ",
     },
     {
-      name: "Agbasieje Peace Chioma",
+      name: "Agbasieje Peace C.",
       role: "UI/UX Designer",
       image: "/team/photo_2024-07-25_02-29-06.jpg",
       github: "https://x.com/oluwa_chioma/status/1786824275749486960?s=46",
-      twitter: "",
-      linkedin: "",
+      twitter: " ",
+      linkedin: " ",
     },
   ];
 
   return (
     <footer className="w-full bg-slate-700 py-12">
-      <div className="px-4">
+      <div>
         <div className="p-2 my-4">
           <h2 className="text-3xl text-center font-bold mb-6 text-[#EEEEEE]">
             Meet Our Team
@@ -65,7 +65,7 @@ const Footer = () => {
             {teamMembers.map((member, index) => (
               <div
                 key={index}
-                className="bg-slate-800 p-4 rounded-lg text-center shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out max-w-64 w-full mx-auto">
+                className="bg-slate-800 p-2 rounded-lg text-center shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out max-w-64 w-full mx-auto text-sm">
                 <Image
                   src={member.image}
                   alt={member.name}
@@ -73,7 +73,9 @@ const Footer = () => {
                   height={1000}
                   className="mx-auto w-32 h-32 border-4 border-gray-900 rounded-full object-cover"
                 />
-                <h5 className="font-bold uppercase text-[#EEEEEE] mt-4">{member.name}</h5>
+                <h5 className="font-bold uppercase text-[#EEEEEE] mt-4">
+                  {member.name}
+                </h5>
                 <p className="text-[#D4D0E0] text-sm">{member.role}</p>
                 <div className="flex justify-center gap-4 mt-4">
                   {member.github && (
